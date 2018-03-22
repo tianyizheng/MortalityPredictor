8b4689de17c124971b4b464efcea8091ef50b716
tianyizheng <tianyizheng93@gmail.com>
2018-03-22 16:30:40 -0400

    icd9 codes now grouped by visits

--- app/app.py
+++ app/app.py
@@ -70,12 +70,13 @@ def chart():
 @app.route('/patient/<patientID>', methods=['GET'])
 def patient(patientID):
   errors = []
-  icdCodes = []
+  icdCodes = {}
   try:
-    icdCodes = icdSearch(patientID)
+    icdCodes = conceptSearch(patientID)
+    keys = sorted(icdCodes)
   except:
     errors.append("error")
-  return render_template('patient.html', errors = errors, codes=icdCodes)
+  return render_template('patient.html', patientID = patientID, errors = errors, codes=icdCodes, keys=keys)
 
 @app.route('/chart2', methods=['GET'])
 def chart2():
@@ -104,28 +105,33 @@ def handle_message(message):
 
   emit('patient data', data)
 
-def icdSearch(patientID):
-  result = []
-  codes = conceptSearch(patientID)
-  for c in codes:
-    concept = Concept.query.filter_by(concept_code=c).limit(1).all()
-    conceptID = concept[0].concept_id
-    icd = ConditionOccurence.query.filter_by(condition_concept_id=conceptID).limit(1).all()
-    result.append(icd[0].condition_source_value)
+def icdSearch(code):
+  concept = Concept.query.filter_by(concept_code=code).limit(1).all()
+  conceptID = concept[0].concept_id
+  icd = ConditionOccurence.query.filter_by(condition_concept_id=conceptID).limit(1).all()
+  result = icd[0].condition_source_value
   return result
 
 def conceptSearch(patientID):
-  codeList = set()
+  result = {}
   search = conditions.Condition.where(struct={'patient': patientID})
   bundle = search.perform(smart.server)
   if bundle.entry:
     for e in bundle.entry:
-      if e.resource.code:
+      if e.resource.encounter and e.resource.code:
+        encounter = e.resource.encounter.reference[10:]
         codes = e.resource.code.coding
-        if codes:
-          for c in codes:
-            codeList.add(c.code)
-  return(codeList)
+        if codes and encounter:
+          if encounter in result:
+            for c in codes:
+              if c.code not in result[encounter]:
+                result[encounter].add(icdSearch(c.code))
+          else:
+            codeList = set()
+            for c in codes:
+              codeList.add(icdSearch(c.code))
+            result[encounter] = codeList
+  return(result)
   
 
 if __name__ == "__main__":
